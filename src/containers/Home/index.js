ddcb42aa8efaa2645d94127179cfd5aab5fc16ff
孙磊 <839228534@qq.com>
2019-10-06 16:39:42 +0800

    do

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../../components/Header'
 
 const Home = () => {
   return (
     <div>
+      <Header />
       This is Peko
       <button onClick={()=>{alert('peko')}}>click</button>
     </div>
--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,13 @@
-import React from 'react'
 import express from 'express'
-import { renderToString } from 'react-dom/server'
-
-import Home from '../containers/Home'
+import { render } from './utils'
 
 const app = express()
-app.use(express.static('public'))
-const content = renderToString(<Home />)
+app.use(express.static('public'))//静态路由
 
-app.get('/',function (req,res) {
-  res.send(`
-  <html>
-   <head>
-   <title>ssr</title>
-   </head>
-   <body>
-     <div id="root">${content}</div>
-   </body>
-   <script src="/index.js"></script>
-  </html>
-  `)
+app.get('*',function (req,res) {
+  res.send(render(req))
 })
+
 
 const server = app.listen('3000',()=>{
   console.log('server is running in localhost3000')
