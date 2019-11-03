5cd73fdb9692cecedff6baf92e883afb9eeafa12
sunlei438 <>
2019-11-03 18:50:27 +0800

    1

--- src/server/request.js
+++ src/server/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const createInstance = (req) => axios.create({
-  baseURL: 'http://47.95.113.63/ssr',
+  baseURL: 'http://47.95.113.63/ssrd',
   headers: {
     cookie: req.get('cookie')
   }
